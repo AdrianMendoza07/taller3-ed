585e0fab074444f7da8d9c235038ca3fc9fa6ef1
MArianA Orti <ortizmariana@uninorte.edu.co>
2026-03-04 16:42:33 +0000

    Agregada clase Clientes

---
+++ Clientes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Clientes {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Clientes (int id, String nombre, String email){
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    @Override 
+    public String toString() {
+        return id + "," + nombre + "," + email;
+    }
+}
