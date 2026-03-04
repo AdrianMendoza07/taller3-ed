a4e24c2bdb33c072472eb6594b74451ec620073d
Adrian Mendoza <68600509+AdrianMendoza07@users.noreply.github.com>
2026-03-04 16:37:39 +0000

    Se creo el constructor de Producto

--- Producto.java
+++ Producto.java
@@ -4,9 +4,15 @@ public class Producto {
     private String nombre;
     private String categoria;
     private int cantidad;
-    private int precio;
+    private int precio; 
 
-    
+    public Producto(int id, String nombre, String categoria, int cantidad, int precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
 
     @Override
     public String toString (){
