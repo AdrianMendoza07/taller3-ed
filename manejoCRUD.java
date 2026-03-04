21f76c5078f005f1099552d356da3acfd8948844
Adrian Mendoza <68600509+AdrianMendoza07@users.noreply.github.com>
2026-03-04 16:55:03 +0000

    a

--- manejoCRUD.java
+++ manejoCRUD.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
+
 class manejoCRUD {
+
     public static void main(String[] args) {
         
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static int leerPrecio()
+            throws IOException {
+        int precio;
+        Scanner sc = new Scanner(new File("productos.csv"));
+
+        while (sc.hasNextLine()) {
+            String[] datos = sc.nextLine().split(",");
+            lista.add(new Usuario(
+                    Integer.parseInt(datos[0]),
+                    datos[1],
+                    datos[2]));
+        }
+        sc.close();
+        return lista;
     }
+}
 }
