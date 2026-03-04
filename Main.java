dc91912667f3c2f0641689eccdaf0103aaa28867
Adrian Mendoza <68600509+AdrianMendoza07@users.noreply.github.com>
2026-03-04 15:59:59 +0000

    Se creo el menu iterativo base en Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese que opcion desea realizar: ");
+        int opcion = sc.nextInt();
+
+        while (opcion != 6) {
+            switch (opcion) {
+                case 6:
+                     
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+
+    }
+}
