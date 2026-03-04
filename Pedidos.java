48e5011c952ebb0232eb4635dec85fa4c98495e8
Nataly Lopez <napologo1010@gmail.com>
2026-03-04 16:24:57 +0000

    Se creo la clase pedidos

---
+++ Pedidos.java
@@ -0,0 +1,11 @@
+public class Pedidos{
+    private int id;
+    private int cliente_id;
+    private int producto_id;
+    private int cantidad;
+    private int fecha;
+
+    public String toString(){
+        return (id+","+cliente_id+","+producto_id+","+cantidad+","+fecha);
+    }
+}
